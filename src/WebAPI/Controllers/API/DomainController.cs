a5cbf9f748773f4437bae233e0ef72b1e43df10d
blackfenix2 <devin_98@msn.com>
2019-10-17 21:53:01 -0400

    added test API controller

---
+++ src/WebAPI/Controllers/API/DomainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.API
+{
+    public class DomainController : ApiController
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new { message = "nothing here yet" });
+        }
+    }
+}
